80e0746c7c1f2182ddcb1d206d08b68f594bc606
Ismail Salah <111834969+IsmailJunior@users.noreply.github.com>
2024-04-11 23:50:36 +0300

    Update Brand.tsx

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import dragonLogo from "@/assets/dragon.png";
 
 function Brand() {
-  return <Image src={dragonLogo} alt="dragon-logo" width={25} />;
+  return <Image src='#' alt="logo" width={25} />;
 }
 
 export default Brand;
